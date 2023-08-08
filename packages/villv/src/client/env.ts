7176897bfe5b67254c0db0af9a09179d79ea999e
Aponia <brianmv@uci.edu>
2023-08-07 21:08:12 -0700

    feat: client env

---
+++ packages/villv/src/client/env.ts
@@ -0,0 +1,36 @@
+//-----------------------------------------------------------------------------------
+// Injected by esbuild's define API.
+//-----------------------------------------------------------------------------------
+declare const __MODE__: string
+
+declare const __DEFINES__: Record<string, any>
+
+const context =
+  typeof globalThis !== 'undefined'
+    ? globalThis
+    : typeof self !== 'undefined'
+    ? self
+    : typeof window !== 'undefined'
+    ? window
+    : Function('return this')()
+
+const defines = __DEFINES__
+
+Object.keys(defines).forEach((key) => {
+  const segments = key.split('.')
+
+  let target = context
+
+  let segment: string
+
+  for (let i = 0; i < segments.length - 1; ++i) {
+    segment = segments[i] ?? ''
+
+    if (i === segments.length - 1) {
+      target[segment] = defines[key]
+    } else {
+      target[segment] ??= {}
+      target = target[segment]
+    }
+  }
+})
